effe330a4797bc04c7d91bbf62368635e45e7a9d
deeqadri <danishqadri79@gmail.com>
2026-02-11 00:59:28 +0000

    Add files via upload

---
+++ lecture5.py
@@ -0,0 +1,13 @@
+#pydoc disscustions how create doceumentation 
+# python3 -m pydoc 
+ipadd="1.1.1.1"
+mask="255.255.255.0"
+#print(ipadd,mask)
+
+con=ipadd+ " "+ mask
+
+print(con)
+
+b='cisco'.upper()
+print(b)
+
---
+++ tuple.py
@@ -0,0 +1,3 @@
+# Tuple(abc)
+# devices(1.1.11.1,username,password)
+
